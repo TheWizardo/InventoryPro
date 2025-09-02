bc0f2c05e34a07e12e60b43c468101abc04dfd75
TheWizardo <39444976+TheWizardo@users.noreply.github.com>
2025-09-02 09:55:10 +0300

    licenseEnd returns null if decryption faliled

--- Backend/src/Logic/license-logic.ts
+++ Backend/src/Logic/license-logic.ts
@@ -3,35 +3,40 @@ import { privateDecrypt, constants } from "crypto";
 import config from "../Utils/config";
 
 class LicenseLogic {
-  public licenseDate: { licenseEnd: Date } = { licenseEnd: new Date(-1) };
+  public licenseDate: { licenseEnd: Date } = { licenseEnd: null };
 
   public decryptJsonMessage<T = any>(
     encryptedMessage: string | Buffer
   ): T {
-    const privateKeyPath = config.assetsFolder + "/privateKey.pem"
-    const privateKey = readFileSync(privateKeyPath, "utf-8");
+    try {
+      const privateKeyPath = config.assetsFolder + "/privateKey.pem"
+      const privateKey = readFileSync(privateKeyPath, "utf-8");
 
-    // Convert encrypted message into Buffer if base64 string
-    const encryptedBuffer =
-      typeof encryptedMessage === "string"
-        ? Buffer.from(encryptedMessage, "base64")
-        : encryptedMessage;
+      // Convert encrypted message into Buffer if base64 string
+      const encryptedBuffer =
+        typeof encryptedMessage === "string"
+          ? Buffer.from(encryptedMessage, "base64")
+          : encryptedMessage;
 
-    // Decrypt using RSA private key
-    const decryptedBuffer = privateDecrypt(
-      {
-        key: privateKey,
-        padding: constants.RSA_PKCS1_OAEP_PADDING,
-        oaepHash: "sha256"
-      },
-      encryptedBuffer as Uint8Array
-    );
+      // Decrypt using RSA private key
+      const decryptedBuffer = privateDecrypt(
+        {
+          key: privateKey,
+          padding: constants.RSA_PKCS1_OAEP_PADDING,
+          oaepHash: "sha256"
+        },
+        encryptedBuffer as Uint8Array
+      );
 
-    // Convert decrypted data to string
-    const decryptedString = decryptedBuffer.toString("utf8");
+      // Convert decrypted data to string
+      const decryptedString = decryptedBuffer.toString("utf8");
 
-    // Parse JSON and return
-    return JSON.parse(decryptedString);
+      // Parse JSON and return
+      return JSON.parse(decryptedString);
+    }
+    catch {
+      return { licenseEnd: "Invalid" } as T;
+    }
   }
 
   public async fetchLicense() {
--- Backend/src/Middleware/licenseValidation-mw.ts
+++ Backend/src/Middleware/licenseValidation-mw.ts
@@ -6,6 +6,9 @@ import catchAll from "./catch-all";
 function validateLicense(req: Request, res: Response, next: NextFunction): void {
     licenseLogic.fetchLicense().then(cipher => {
         const dateString = licenseLogic.decryptJsonMessage<{ licenseEnd: string }>(cipher).licenseEnd;
+        if (dateString === "Invalid") {
+            throw new UnauthorizedError("Invalid License");
+        }
         const date = new Date(dateString);
         licenseLogic.licenseDate = { licenseEnd: date };
         const now = new Date();
