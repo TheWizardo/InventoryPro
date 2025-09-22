2bf34b1bb0cb87316cc28ec757297e15a9536727
TheWizardo <39444976+TheWizardo@users.noreply.github.com>
2025-09-22 13:30:22 +0300

    logging connection uri

--- Backend/src/Utils/dal.ts
+++ Backend/src/Utils/dal.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import config from "./config";
 
-const mongoUri = process.env?.MONGO_URI || "mongodb://localhost:27017/" + config.client;
+const mongoUri = process.env?.MONGO_URI || "mongodb://localhost:27017/" + process.env?.CLIENT;
 
 /**
  * Connect to MongoDB using Mongoose.
@@ -13,7 +13,7 @@ export async function connectToDatabase(): Promise<void> {
 
     try {
         await mongoose.connect(mongoUri);
-        console.log("✅ Connected to MongoDB with Mongoose");
+        console.log("✅ Connected to MongoDB with Mongoose @ " + mongoUri);
     } catch (error) {
         console.error("❌ MongoDB connection error:", error);
         throw error;
