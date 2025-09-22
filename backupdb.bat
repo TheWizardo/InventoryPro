3748821c1276428981fc3c4df9cbbf44a2dba7d6
TheWizardo <39444976+TheWizardo@users.noreply.github.com>
2025-09-22 13:20:22 +0300

    migrated to backing up using mongoexport

--- backupdb.bat
+++ backupdb.bat
@@ -6,6 +6,7 @@ set CONTAINER_NAME=mongo
 set DB_NAME=<db_Name>
 set HOST_BACKUP_DIR=%programdata%\InventoryPro\mongo_backups
 set CONTAINER_BACKUP_DIR=/backup
+SET COLLECTIONS=Inventory Assembly InventoryLog Employees Projects
 
 REM === Get timestamp in YYYY-MM-DD_HH-MM ===
 for /f "skip=1" %%x in ('wmic os get localdatetime') do if not defined mydate set mydate=%%x
@@ -28,11 +29,15 @@ echo host dir %HOST_TIMESTAMP_DIR%
 
 REM === CLEAR CONTAINER BACKUP FOLDER ===
 echo Clearing container backup folder: %CONTAINER_BACKUP_DIR%
-docker exec %CONTAINER_NAME% rm -rf %CONTAINER_BACKUP_DIR%/*
+docker exec %CONTAINER_NAME% rm -rf %CONTAINER_BACKUP_DIR%
+docker exec %CONTAINER_NAME% mkdir %CONTAINER_BACKUP_DIR%
 
 REM === CREATE BACKUP INSIDE CONTAINER ===
 echo Creating backup in container...
-docker exec %CONTAINER_NAME% mongodump --db %DB_NAME% --out %CONTAINER_BACKUP_DIR%
+FOR %%c IN (%COLLECTIONS%) DO (
+    echo exporting %%c
+    docker exec %CONTAINER_NAME% bash -c "mongoexport --db %DB_NAME% --collection %%c --out %CONTAINER_BACKUP_DIR%/%%c.json --jsonArray"
+)
 
 REM === COPY TO HOST MACHINE ===
 echo Copying backup to host folder: %HOST_TIMESTAMP_DIR%
